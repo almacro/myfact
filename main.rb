6c3cc70a202a78faa574ac05788fa974306dcaab
Al Thompson <almacro@acm.org>
2017-09-22 11:18:56 -0700

    Implemented recursive fact (again!) This revision adds back the recursv  impl.

--- main.rb
+++ main.rb
@@ -1,1 +1,13 @@
-puts "Hello World!"
+n, rest_args = ARGV
+
+def factorial(n)
+  if n == 0
+    return 1
+  else
+    return n*factorial(n - 1)
+  end
+end
+
+n = n.to_i
+puts "#{n}! = #{factorial(n)}"
+
